5a819b1c26c6d2a756a46f16936682dc48cba452
徐晓刚 <xxg209495@alibaba-inc.com>
2020-08-31 17:00:39 +0800

    Updated: get error module

--- src/js/getErrorReason.js
+++ src/js/getErrorReason.js
@@ -38,16 +38,22 @@ function getReason(message) {
     return '运行错误：页面加载超时';
   }
   if (message.includes('CypressError') && message.includes('`cy.request()` failed')) {
-    return '运行错误：请求异常'
+    return '运行错误：请求异常';
   }
   if (message.includes('CypressError') && message.includes('timed out waiting')) {
-    return '运行错误：执行超时'
+    return '运行错误：执行超时';
   }
   if (message.includes('CypressError') && message.includes('Timed out retrying')) {
-    return '运行错误：重试超时'
+    return '运行错误：重试超时';
   }
   if (message.includes('CypressError') && message.includes('can only be called on a single element')) {
-    return '运行错误：元素处理异常'
+    return '运行错误：元素处理异常';
+  }
+  if (message.includes('is not a function')) {
+    return '函数错误：使用的函数异常';
+  }
+  if (message.includes('is not defined at')) {
+    return '申明错误：使用了未申明的变量或函数';
   }
   return message;
 }
